8583288a714912acdd094c8d823d28e30f45615b
wu.julia573@gmail.com <wu.julia573@gmail.com>
2025-04-17 19:39:37 -0400

    .

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <map>
+#include <fstream>
 using namespace std;
 
 int main(){
-    cout<<"hii";
+
+    map<int, string> characters;
+    ifstream file("characters.txt");
+    if (!file.is_open())
+    {
+        cout << "File could not be opened" << endl;
+        return 0;
+    }
+    string s;
+    for (int i = 0; i<41; i++)
+    {
+        getline(file, s);
+        //cout<<s<<endl;
+        characters[i] = s;
+    }
+    file.close();
+
     return 0;
 }
